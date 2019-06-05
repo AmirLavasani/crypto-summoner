e437f70c25095eab83508cf5af1f85224ede40c6
Amir <amirm.lavasani@gmail.com>
2019-06-05 23:01:54 +0430

    add a colorful logger :sparkles:

--- crypto_summoner/inteli_logger/inteli_logger.py
+++ crypto_summoner/inteli_logger/inteli_logger.py
@@ -1,8 +1,8 @@
 """
     InteliLogger is a class for customized logging.
-    
+
     Any other class can log their activity using this class.
-    
+
 """
 
 import logging
@@ -23,31 +23,31 @@ class InteliLogger:
 
     Attributes:
         _registered_loggers (dic): Dictionary of initialized loggers.
-    
-    Methods:       
+
+    Methods:
         get_logger(cls, logger_name): Returns the logger with logger_name.
-        _add_stream_handler(cls, logger_name): Adds a stream handler to the logger with logger_name. 
+        _add_stream_handler(cls, logger_name): Adds a stream handler to the logger with logger_name.
         _add_file_handler(cls, logger_name): Adds a file handler to the logger with logger_name.
     """
-    _registered_loggers = {}       
+    _registered_loggers = {}
 
     @classmethod
     def get_logger(cls, logger_name):
         """
-        Returns the logger with logger_name. 
-        
+        Returns the logger with logger_name.
+
         If the logger_name does not exists it creates the logger.
         If the logger_name exists in _registered_loggers it return the same object.
 
-        By default it adds two handler to logger object. 
+        By default it adds two handler to logger object.
         A file handler which writes to INTELI_LOGGER_LOG_FILE path, and
         A stream handler which writes log to std_out.
-               
+
         Args:
-            logger_name (str): Name of the logger.            
-        
+            logger_name (str): Name of the logger.
+
         Returns:
-            Returns a logging.Logger instance.        
+            Returns a logging.Logger instance.
         """
 
         if logger_name not in cls._registered_loggers.keys():
@@ -57,38 +57,38 @@ class InteliLogger:
             cls._add_file_handler(logger_name)
 
         return cls._registered_loggers[logger_name]
-    
+
     @classmethod
     def _add_stream_handler(cls, logger_name):
         """
         Add a logging.StreamHandler to the logger with logger_name
 
-        Using ColoredFormatter from colorlog the stream output will be colored.      
-               
+        Using ColoredFormatter from colorlog the stream output will be colored.
+
         Args:
-            logger_name (str): Name of the logger.            
-        
+            logger_name (str): Name of the logger.
+
         Returns:
-            Returns nothing.        
+            Returns nothing.
         """
 
         stream_handler = logging.StreamHandler()
         stream_handler.setLevel(INTELI_LOGGER_STREAM_LOGGING_LEVEL)
         stream_format = ColoredFormatter(INTELI_LOGGER_STREAM_FORMAT)
-        stream_handler.setFormatter(stream_format)        
+        stream_handler.setFormatter(stream_format)
         logger = cls._registered_loggers[logger_name]
         logger.addHandler(stream_handler)
-    
+
     @classmethod
     def _add_file_handler(cls, logger_name):
         """
-        Add a logging.FileHandler to the logger with logger_name        
-               
+        Add a logging.FileHandler to the logger with logger_name
+
         Args:
-            logger_name (str): Name of the logger.            
-        
+            logger_name (str): Name of the logger.
+
         Returns:
-            Returns nothing.        
+            Returns nothing.
         """
 
         file_handler = logging.FileHandler(INTELI_LOGGER_LOG_FILE)
@@ -104,7 +104,7 @@ def test_inteli_logger():
     log2 = InteliLogger.get_logger('log2')
     log3 = InteliLogger.get_logger('log1')
     assert log1 is log3
-    
+
     log1.debug('This is a debug message')
     log1.info('This is an info message')
     log1.warning('This is a warning message')
@@ -112,10 +112,11 @@ def test_inteli_logger():
     log1.critical('This is a critical message')
 
     log2.debug('This is a debug message')
-    log2.info('This is an info message')    
+    log2.info('This is an info message')
     log2.warning('This is a warning message')
     log2.error('This is an error message')
     log2.critical('This is a critical message')
+
 
 if __name__ == "__main__":
     test_inteli_logger()
