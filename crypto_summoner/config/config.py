92f2865d2b323b4faef241dbb044a2fcd6757da5
Amir <amirm.lavasani@gmail.com>
2019-07-03 21:51:20 +0430

    correct style :art:

--- crypto_summoner/config/config.py
+++ crypto_summoner/config/config.py
@@ -1,7 +1,8 @@
 """
     Config is a class for accessing an encrypted config file.
-    
-    It will decrypt the YAML config file and provides other classes with their configurations.
+
+    It will decrypt the YAML config file and provides other
+     classes with their configurations.
 
 """
 
@@ -10,44 +11,53 @@ import os
 import yaml
 
 from crypto_summoner.inteli_logger.inteli_logger import InteliLogger
+
 logger = InteliLogger.get_logger('Config')
 
 
 class Config:
     """
     Config class
-    
-    Config is a class for accessing and storing an encrypted config file.    
-    It will decrypt the YAML config file and provides other classes with their configurations.
+
+    Config is a class for accessing and storing an encrypted config file.
+    It will decrypt the YAML config file and provides other classes
+     with their configurations.
+
     Design Pattern: Configuration :D
 
     ...
 
     Attributes:
         CONFIGURATION (dic): Dictionary of key values.
-    
-    Methods:       
-        get_property(cls, property_name): Returns the value of requested property_name.
-        decrypt_configuration(cls): Decrypt configuration file and set class CONFIGURATION member.
-    """
-    CONFIGURATION = {}       
 
-    @classmethod  
+    Methods:
+        get_property(cls, property_name): Returns the value
+         of requested property_name.
+
+        decrypt_configuration(cls): Decrypt configuration file
+         and set class CONFIGURATION member.
+
+    """
+
+    CONFIGURATION = {}
+
+    @classmethod
     def get_property(cls, property_name):
         """
         Returns the value of requested property_name.
-        
+
         Args:
             property_name (str): Name of property.
-        
+
         Returns:
             If successfull returns a property value, otherwise None.
+
         """
-        
+
         # obtain and set configuration data one time.
         if cls.CONFIGURATION == {}:
             cls.obtain_configuration()
-        
+
         if property_name not in cls.CONFIGURATION.keys():
             return None
         return cls.CONFIGURATION[property_name]
@@ -57,18 +67,21 @@ class Config:
         """
         Obtain configuration data and set CONFIGURATION class member.
 
-        It assumes an environment variable CRYPTO_SUMMONER_CONFIG_FILE is set to YAML configuration file.
-        
+        It assumes an environment variable CRYPTO_SUMMONER_CONFIG_FILE
+         is set to YAML configuration file.
+
         Returns:
             Returns nothing.
-        
+
         Raises:
-            ValueError: This function must be override in derived classes  
+            ValueError: This function must be override in derived classes
+
         """
+
         if "CRYPTO_SUMMONER_CONFIG_FILE" not in os.environ:
             logger.exception("CRYPTO_SUMMONER_CONFIG_FILE environment variable is not set.")
             raise ValueError("CRYPTO_SUMMONER_CONFIG_FILE environment variable is not set.")
-        
+
         yaml_config_file = os.getenv("CRYPTO_SUMMONER_CONFIG_FILE")
         decrypted_config_data = cls.decrypt_configuration(yaml_config_file)
 
@@ -79,23 +92,24 @@ class Config:
             raise ValueError("Failed to safe_load config file.")
 
         # yaml_configs is a list of dictionaries
-        for config in yaml_configs:           
+        for config in yaml_configs:
             cls.CONFIGURATION.update(config)
 
     @classmethod
     def decrypt_configuration(cls, yaml_config_file):
         """
-        Decrypt configuration data and return the data.        
-        
+        Decrypt configuration data and return the data.
+
         Args:
             yaml_config_file (str): YAML configuration file path.
 
         Returns:
             Returns configuration data.
-        
+
         Raises:
             ValueError: If Configuration YAML file not exists.
             # TODO raise if it can not decrypt data or decryption key is not valid.
+
         """
 
         if not os.path.exists(yaml_config_file):
@@ -110,6 +124,7 @@ class Config:
 def main():
     Config.obtain_configuration()
     print(Config.CONFIGURATION)
+
 
 if __name__ == "__main__":
     main()
